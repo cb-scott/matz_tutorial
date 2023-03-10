1a8993dd860108c21a471264aee16e051f2c9d6b
cb-scott <cbscott@utexas.edu>
2023-03-10 12:21:49 -0600

    trydada

--- tutorials/Mar10_Dada2_local.R
+++ tutorials/Mar10_Dada2_local.R
@@ -5,4 +5,9 @@ devtools::install_github("benjjneb/dada2", ref="v1.16")
 
 library("dada2")
 
-path <- "~/Downloads/pairedfastq/" #be sure to unzip the file 
+path <- "~/Downloads/pairedfastq" #be sure to unzip the file 
+list.files(path)
+fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+plotQualityProfile(fnFs[1:2])
