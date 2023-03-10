288bd74291b88c1d27ec51dfe8904ad74cb5ee11
cb-scott <cbscott@utexas.edu>
2023-03-10 11:57:12 -0600

    trydada

--- tutorials/Mar10_Dada2_local.R
+++ tutorials/Mar10_Dada2_local.R
@@ -1,3 +1,8 @@
+#Only need to run these on the first time 
 install.packages("devtools")
 library("devtools")
-devtools::install_github("benjjneb/dada2", ref="v1.16") 
+devtools::install_github("benjjneb/dada2", ref="v1.16")
+
+library("dada2")
+
+path <- "~/Downloads/pairedfastq/" #be sure to unzip the file 
