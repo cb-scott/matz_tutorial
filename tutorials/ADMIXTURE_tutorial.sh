dfe5b5bb9311a01cbb1b1a0a882e932e909f9ca1
cb-scott <cbscott@utexas.edu>
2023-03-31 11:42:11 -0500

    idev

--- tutorials/ADMIXTURE_tutorial.sh
+++ tutorials/ADMIXTURE_tutorial.sh
@@ -1,3 +1,4 @@
 conda activate your_environment
 conda install pcangsd #this might have conflicts
+idev -A IBN21018 -t 02:00:00
 pcangsd -beagle YOURNAME.beagle.gz -admix -o pcangsd -inbreed 2 -kinship -selection -threads 12
